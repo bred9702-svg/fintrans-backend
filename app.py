96476be565236855e0258c03c920cd90cf5b1677
bred9702-svg <bred9702@gmail.com>
2026-05-09 05:53:51 +0300

    Update and rename app. py to app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "MuniaPay Backend Live 🚀"
+
+@app.route('/transactions')
+def transactions():
+    return jsonify([])
