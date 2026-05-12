3d6d7296da8e6c46df650aa3c1192879d88d49d7
bred9702-svg <bred9702@gmail.com>
2026-05-12 19:52:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ RATES = {
 }
 
 CORRESPONDENTS = {
-    "RDC_TO_KEN": {"deposit": "AIRTEL_MONEY_COD", "payout": "MPESA_KEN"},
-    "KEN_TO_RDC": {"deposit": "MPESA_KEN", "payout": "AIRTEL_MONEY_COD"}
+    "RDC_TO_KEN": {"deposit": "AIRTEL_COD", "payout": "MPESA_KEN"},
+    "KEN_TO_RDC": {"deposit": "MPESA_KEN", "payout": "AIRTEL_COD"}
 }
 
 CURRENCIES = {
